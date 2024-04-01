2c36859ab9486d0bad785f59b6c1842591912f69
computer6561 <computer6561@bluehole.net>
2024-04-01 14:05:17 +0900

    C++ 스크립트 설정 세팅

---
+++ Source/EDCardEffect/EDCardEffect.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EDCardEffect.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EDCardEffect, "EDCardEffect" );
