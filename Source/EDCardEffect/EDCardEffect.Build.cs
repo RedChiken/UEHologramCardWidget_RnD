7310c7d7fed98be8bcc0d06a78906a8063e0753c
computer6561 <computer6561@bluehole.net>
2024-04-01 14:25:47 +0900

    Spine Plugin 세팅

--- Source/EDCardEffect/EDCardEffect.Build.cs
+++ Source/EDCardEffect/EDCardEffect.Build.cs
@@ -8,7 +8,7 @@ public class EDCardEffect : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SpinePlugin", "ProceduralMeshComponent" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
